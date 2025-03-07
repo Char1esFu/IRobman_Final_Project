c0f6368a66070b737faac4442aa325a8b4d8109c
Charles Fu <charles940316@gmail.com>
2025-03-07 22:25:08 +0100

    starter code fix

--- src/robot.py
+++ src/robot.py
@@ -58,8 +58,11 @@ class Robot:
             p.changeDynamics(self.id, j, linearDamping=0, angularDamping=0)
 
     def set_default_position(self):
-        for idx, pos in zip(self.arm_idx, self.default_arm):
+        for idx, pos in zip(self.arm_idx, self.default_arm[:-2]):
             p.resetJointState(self.id, idx, pos)
+
+        for grp_idx, pos in zip(self.gripper_idx, self.default_arm[-2:]):
+            p.resetJointState(self.id, grp_idx, pos)
 
     def get_joint_limits(self):
         lower = []
